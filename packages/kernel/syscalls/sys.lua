6902e930ec4395c3d1282cd36b595b3a4c909649
_lordBucket <antoniuk.orest@gmail.com>
2026-02-07 22:09:58 +0200

    sys.epoch, sys.version

--- packages/kernel/syscalls/sys.lua
+++ packages/kernel/syscalls/sys.lua
@@ -3,6 +3,20 @@ local ProcessRegistry = require("process.ProcessRegistry");
 
 local sys = {};
 
+---Returns number of milliseconds since an epoch depending on locale.
+---@param tcb Thread Thread calling the syscall.
+---@param locale string locale to get time for, defaults to `ingame` if not set.
+function sys.epoch(tcb, locale)
+    return os.epoch(locale);
+end
+
+---Sets an alarm for a specific in-game time (0.0 to 24.0).
+---@param tcb Thread Thread calling the syscall.
+---@param time number Time for alarm to be set to.
+function sys.alarm(tcb, time)
+    -- Implementation TODO: os.setAlarm(time)
+end
+
 ---Starts a system timer that fires after `duration` seconds.
 ---@param tcb Thread Thread calling the syscall.
 ---@param fd number File descriptor pointing to port, that timer should send message to.
@@ -23,7 +37,81 @@ function sys.cancel(tcb, id)
     return TimerManager.cancel(pcb, id);
 end
 
+---Writes a message to kernel ring buffer.
+---@param tcb Thread Thread calling the syscall.
+---@param level string Log severity ("INFO", "WARN", "ERROR").
+---@param msg string Message content.
+function sys.log(tcb, level, msg)
+    -- Implementation TODO: Write to kernel buffer
+end
+
+---Returns a table containing global system information.
+---@param tcb Thread Thread calling the syscall.
+function sys.info(tcb)
+    local Scheduler = require("Scheduler");
+    local times = Scheduler.getTimeUsage();
+
+    return {
+        version = require("version").string; -- os version
+        hardware = os.version(); -- cc version
+        build = require("version").build, -- os build
+        uptime = os.clock(); -- time since computer was started
+        startEpoch = times.startEpoch; -- time since scheduler started running
+        runningTime = times.runningTime; -- time spent running user processes
+        systemTime = times.systemTime; -- time spent running syscalls (kernel time)
+        idleTime = times.idleTime; -- time spent waiting for hardware events
+    }
+end
+
+---Subscribes a port to a specific raw ComputerCraft event type.
+---Once bound, kernel intercepts all events of this type and routes them exclusively to the specified port.
+---Requires root.
+---@param tcb Thread Thread calling the syscall.
+---@param event string Event name.
+---@param port number Handle id of the port to receive events.
+function sys.bind_event(tcb, event, port)
+    -- Implementation TODO: Register event listener in kernel
+end
+
+---Releases the exclusive subscription for an event type.
+---Requires root.
+---@param tcb Thread Thread calling the syscall.
+---@param event string Event name.
+function sys.unbind_event(tcb, event)
+    -- Implementation TODO: Unregister event listener
+end
+
+---Shuts down the computer. Requires root permissions.
+---@param tcb Thread Thread calling the syscall.
+function sys.shutdown(tcb)
+    local process = ProcessRegistry.get(tcb.pid);
+    if (process.uid ~= 0) then
+        error("EPERM: No permission.");
+    end
+
+    os.shutdown();
+end
+
+---Reboots the computer. Requires root permissions.
+---@param tcb Thread Thread calling the syscall.
+function sys.reboot(tcb)
+    local process = ProcessRegistry.get(tcb.pid);
+    if (process.uid ~= 0) then
+        error("EPERM: No permission.");
+    end
+
+    os.reboot();
+end
+
 return {
+    [96] = sys.epoch,
     [97] = sys.timer,
+    [98] = sys.alarm,
     [99] = sys.cancel,
+    [100] = sys.log,
+    [101] = sys.info,
+    [102] = sys.bind_event,
+    [103] = sys.unbind_event,
+    [104] = sys.shutdown,
+    [105] = sys.reboot,
 }
